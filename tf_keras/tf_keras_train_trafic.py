d8210e396c8a13d1756879db48089be9a7380029
z <phxv@mail.ru>
2019-04-09 02:57:54 +0300

    1

--- tf_keras/tf_keras_train_trafic.py
+++ tf_keras/tf_keras_train_trafic.py
@@ -140,7 +140,7 @@ print(model.summary())
 history = model.fit_generator(
         train_generator,
         steps_per_epoch=len(train_generator.filenames) // train_generator.batch_size,
-        epochs=50,
+        epochs=30,
         validation_data=validation_generator,
         validation_steps=len(validation_generator.filenames) // validation_generator.batch_size)
 #model.save_weights('first_try.h5')  # always save your weights after training or during training
