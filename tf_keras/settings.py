75d04e87ea4e5b2d885ebe6eab6c65f52d3ba51f
z <phxv@mail.ru>
2019-04-08 03:10:54 +0300

    1

--- tf_keras/settings.py
+++ tf_keras/settings.py
@@ -4,5 +4,5 @@ if os.path.exists('.local'):
 	train_data_path = '/mnt/lin2/datasets/natural_images/'
 	validation_data_path = '/mnt/lin2/datasets/natural_images/'
 else:
-	train_data_path = '/home/andrei/work/t7_cv/dataset_plants/splited/train'
-	validation_data_path = '/home/andrei/work/t7_cv/dataset_plants/splited/valid'
+	train_data_path = '/home/andrei/work/t7_cv/trafic_signs/splited/train'
+	validation_data_path = '/home/andrei/work/t7_cv/trafic_signs/splited/valid'
