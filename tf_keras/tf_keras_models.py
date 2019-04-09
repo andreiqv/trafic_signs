4e6779853f3caefbf938a7e5177a4cb45a12ffe0
z <phxv@mail.ru>
2019-04-10 01:10:51 +0300

    1

--- tf_keras/tf_keras_models.py
+++ tf_keras/tf_keras_models.py
@@ -13,12 +13,12 @@ from tensorflow.keras.applications.resnet50 import ResNet50
 OUTPUT_NAME = 'output'
 
 
-def model_ResNet50(inputs):
+def model_ResNet50(inputs, num_classes=1):
 
 	base_model = ResNet50(weights='imagenet', include_top=False, 
 		pooling='avg', input_tensor=inputs)
 	x = base_model.output
-	x = layers.Dense(5, activation='sigmoid', name=OUTPUT_NAME)(x)
+	x = layers.Dense(num_classes, activation='sigmoid', name=OUTPUT_NAME)(x)
 	model = keras.Model(inputs=inputs, outputs=x, name='keras_ResNet50')	
 	return model
 
